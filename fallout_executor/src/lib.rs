730fd9c5f5e82021ea1f1df18c3911768bf80a16
Zacharie Corniere <zacharie.corniere@epitech.eu>
2020-11-06 19:28:33 +0100

    chore(executor): move to a separate file

--- fallout_executor/src/lib.rs
+++ fallout_executor/src/lib.rs
@@ -6,6 +6,8 @@ extern crate alloc;
 pub mod keyboard;
 pub mod simple_executor;
 pub mod task;
+mod executor;
+pub use self::executor::Executor;
 pub use self::task::Task;
 
 use core::sync::atomic::{AtomicU64, Ordering};
@@ -20,107 +22,3 @@ impl TaskId {
     }
 }
 
-use alloc::{collections::BTreeMap, sync::Arc};
-use core::task::Waker;
-use core::task::{Context, Poll};
-use crossbeam_queue::ArrayQueue;
-
-pub struct Executor {
-    tasks: BTreeMap<TaskId, Task>,
-    task_queue: Arc<ArrayQueue<TaskId>>,
-    waker_cache: BTreeMap<TaskId, Waker>,
-}
-
-impl Executor {
-    pub fn new() -> Self {
-        Executor {
-            tasks: BTreeMap::new(),
-            task_queue: Arc::new(ArrayQueue::new(100)),
-            waker_cache: BTreeMap::new(),
-        }
-    }
-
-    pub fn spawn(&mut self, task: Task) {
-        let task_id = task.id;
-        if self.tasks.insert(task.id, task).is_some() {
-            panic!("task with the same ID already in the task")
-        }
-        self.task_queue.push(task_id).expect("queue full");
-    }
-
-    fn run_ready_tasks(&mut self) {
-        let Self {
-            tasks,
-            task_queue,
-            waker_cache,
-        } = self;
-
-        while let Ok(task_id) = task_queue.pop() {
-            let task = match tasks.get_mut(&task_id) {
-                Some(e) => e,
-                None => continue,
-            };
-            let waker = waker_cache
-                .entry(task_id)
-                .or_insert_with(|| TaskWaker::new(task_id, task_queue.clone()));
-            let mut context = Context::from_waker(waker);
-            match task.poll(&mut context) {
-                Poll::Ready(()) => {
-                    tasks.remove(&task_id);
-                    waker_cache.remove(&task_id);
-                }
-                Poll::Pending => {}
-            }
-        }
-    }
-
-    fn sleep_if_idle(&self) {
-        use x86_64::instructions::interrupts::{self, enable_interrupts_and_hlt};
-
-        interrupts::disable();
-        if self.task_queue.is_empty() {
-            enable_interrupts_and_hlt();
-        } else {
-            interrupts::enable();
-        }
-    }
-
-    pub fn run(&mut self) -> ! {
-        loop {
-            self.run_ready_tasks();
-            self.sleep_if_idle();
-        }
-    }
-}
-
-use alloc::task::Wake;
-
-struct TaskWaker {
-    task_id: TaskId,
-    task_queue: Arc<ArrayQueue<TaskId>>,
-}
-
-impl TaskWaker {
-    fn new(task_id: TaskId, task_queue: Arc<ArrayQueue<TaskId>>) -> Waker {
-        Waker::from(Arc::new(TaskWaker {
-            task_id,
-            task_queue,
-        }))
-    }
-
-    fn wake_task(&self) {
-        self.task_queue
-            .push(self.task_id)
-            .expect("tasks_queue full");
-    }
-}
-
-impl Wake for TaskWaker {
-    fn wake(self: Arc<Self>) {
-        self.wake_task();
-    }
-
-    fn wake_by_ref(self: &Arc<Self>) {
-        self.wake_task();
-    }
-}
